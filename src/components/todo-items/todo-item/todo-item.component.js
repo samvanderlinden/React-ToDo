a74bb785bd47250a52dd35ff6f10a8bca1a89d0b
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-02-17 22:31:17 -0600

    clean up code comments in todo-item and todo-items components

--- src/components/todo-items/todo-item/todo-item.component.js
+++ src/components/todo-items/todo-item/todo-item.component.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const ToDoItem = (props) => {
-    console.log({props});
     return <div>{props.currentToDo} <button onClick={() => {props.deleteToDo(props.uniqueID)}}>Delete</button></div>
 }
 
--- src/components/todo-items/todo-items.component.js
+++ src/components/todo-items/todo-items.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ToDoItem from './todo-item/todo-item.component';
+// import ToDoForm from '../todo-form/todo-form.component';
 
 export default class ToDoItems extends Component {
     constructor(props) {
@@ -8,6 +9,7 @@ export default class ToDoItems extends Component {
         this.state = {
             todos: [],
             isComplete: false,
+            todo: ''
         }
     }
 
@@ -23,24 +25,53 @@ export default class ToDoItems extends Component {
 
     todosList = () => {
         return this.state.todos.map(todo => {
-            console.log({todo});
-            return <ToDoItem key={todo._id} uniqueID={todo._id} currentToDo={todo.todo} deleteToDo = {this.deleteToDo} />
+            return <ToDoItem key={todo._id} uniqueID={todo._id} currentToDo={todo.todo} deleteToDo={this.deleteToDo} />
         });
     }
 
     deleteToDo = (id) => {
         const url = `http://localhost:5000/todos/${id}`;
         axios.delete(url)
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err));
-        this.setState({todos:this.state.todos.filter(el => el._id !== id)});
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err));
+        this.setState({ todos: this.state.todos.filter(el => el._id !== id) });
     }
+
+    //START OF POST COMPONENT
+    handleChange = (e) => {
+        this.setState({ todo: e.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const todo = {
+            todo: this.state.todo
+        }
+        const url = 'http://localhost:5000/todos/add';
+        axios.post(url, todo)
+            .then(res => console.log(res.data));
+
+        this.setState({ 
+            todo: '',
+        });
+    }
+    //END POST COMPONENT
 
     render() {
         return (
             <div>
                 <h1>ToDoItems</h1>
                 {this.todosList()}
+                
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            To Do:
+                        <input type="text" value={this.state.todo} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
         );
     }
